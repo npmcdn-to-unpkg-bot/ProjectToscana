56ef4bcee4825f7902972ce935f251b74898c0f9
jhdv <n8bfgjfs>
2016-05-02 15:05:59 +0200

    Upload-Type-Check

--- actions/upload-action.php
+++ actions/upload-action.php
@@ -7,41 +7,47 @@ $storeFolder = '../uploads/';
 
 $storeThumbnailFolder = "";
 
+$allowedTypes = ["jpg", "jpeg", "png", "tiff", "bmp", "gif"];
+
 if (!empty($_FILES)) {
 
  $tempFile = $_FILES['file']['tmp_name'];
 
  $name = $_FILES['file']['name'];
-
- $id = uniqid();
-
  $type = pathinfo($name, PATHINFO_EXTENSION);
 
- $size = getimagesize($tempFile);
+ if (in_array(strtolower($tyoe), $allowedTypes)) {
 
- $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;
 
- $targetFile = $targetPath.$id.".".$type;
- $targetThumbnailFile = $targetPath.$id."_thumbnail.".$type;
- $targetPreviewFile = $targetPath.$id."_preview.".$type;
+  $id = uniqid();
 
- if (move_uploaded_file($tempFile,$targetFile)) {
 
-  makePreview($targetFile, $targetThumbnailFile, 350);
-  makePreview($targetFile, $targetPreviewFile, 1280);
+  $size = getimagesize($tempFile);
 
-  $sql = "INSERT INTO fotos (UID, ID, type) VALUES ('', '".$id."', '".$type."')";
+  $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;
 
-  if ($mysqli->query($sql) === TRUE) {
-   echo "New record created successfully";
-  } else {
-   echo "Error: " . $sql . "<br>" . $mysqli->error;
+  $targetFile = $targetPath.$id.".".$type;
+  $targetThumbnailFile = $targetPath.$id."_thumbnail.".$type;
+  $targetPreviewFile = $targetPath.$id."_preview.".$type;
+
+  if (move_uploaded_file($tempFile,$targetFile)) {
+
+   makePreview($targetFile, $targetThumbnailFile, 350);
+   makePreview($targetFile, $targetPreviewFile, 1280);
+
+   $sql = "INSERT INTO fotos (UID, ID, type) VALUES ('', '" . $id . "', '" . $type . "')";
+
+   if ($mysqli->query($sql) === TRUE) {
+    echo "New record created successfully";
+   } else {
+    echo "Error: " . $sql . "<br>" . $mysqli->error;
+   }
+
+   $mysqli->close();
   }
 
-  $mysqli->close();
 
-
- }
+  }
 
 }
 
